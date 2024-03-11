6f6c45c5121a317c3e136e519b50073ad7bd0e8e
praveen pugazh <praveenpugazh14@gmail.com>
2024-03-11 18:56:23 +0530

    feat: little styling changes

--- src/components/Views/Contact.jsx
+++ src/components/Views/Contact.jsx
@@ -1,40 +1,70 @@
-
+// import { useState } from 'react'
 const Contact = () => {
   return (
-    <div className="w-[96%] m-auto">
-        <h2 className="text-6xl font-bold my-28">Contact</h2>
-        <form action="">
-            <div>
-                <label htmlFor="FirstName" className="inline-block w-40">First Name</label>
-                <input type="text" className="ml-3 p-3 border border-black border-solid outline-black mr-5" />
-                <label htmlFor="LastName" className="inline-block w-32">Last Name</label>
-                <input type="text" className="mx-3 p-3 border border-black border-solid outline-black" />
-            </div>
-            <br />
-            <div>
-                <label htmlFor="Gender">
-                    Gender
-                </label>
-                <br />
-                <div className="my-3">
-                    <input type="radio" value="male" name="gender" /> <span className="mr-5">Male</span>
-                    <input type="radio" value="female" name="gender"/> Female
-                </div>
-                <label htmlFor="Email" className="inline-block w-40">Email</label>
-                <input type="email" className="mx-3 p-3 border border-black border-solid outline-black" />
-            </div>
-            <br />
-            <div>
-                <label htmlFor="Number" className="inline-block w-40">Mobile</label>
-                <input type="text" className="mx-3 p-3 border border-black border-solid outline-black" />
-            </div>
-            <br />
-            <div>
-                <button className='px-6 py-3 bg-[#E5BE7F] text-xl rounded-lg font-bold'>Submit</button>
-            </div>
+    <div className='w-[96%] m-auto'>
+      <h2 className='text-6xl font-bold my-28 text-center'>Contact</h2>
+      <div className='w-[760px] m-auto bg-[#0C3948] p-10 rounded-md text-gray-50'>
+        <form action=''>
+          <div>
+            <label htmlFor='FirstName' className='inline-block w-20'>
+              First Name
+            </label>
+            <input
+              type='text'
+              className='ml-3 p-3 border border-black border-solid  mr-5 text-gray-950 rounded-md outline-none'
+            />
+            <label htmlFor='LastName' className='inline-block w-20'>
+              Last Name
+            </label>
+            <input
+              type='text'
+              className='mx-3 p-3 border border-black border-solid  text-gray-950 rounded-md outline-none'
+            />
+          </div>
+          <br />
+          <div>
+            <label htmlFor='Email' className='inline-block w-20'>
+              Email
+            </label>
+            <input
+              type='email'
+              className='mx-3 p-3 border border-black border-solid  text-gray-950 rounded-md outline-none'
+            />
+          </div>
+          <br />
+          <div>
+            <label htmlFor='Number' className='inline-block w-20'>
+              Mobile
+            </label>
+            <input
+              type='text'
+              className='mx-3 p-3 border border-black border-solid  text-gray-950 rounded-md outline-none'
+            />
+          </div>
+          <br />
+          <div>
+            <label htmlFor='Number' className='inline-block w-20'>
+              Course
+            </label>
+            <select
+              name='course'
+              id='course'
+              className='mx-3 p-3 border border-black border-solid  text-gray-950 rounded-md outline-none'
+            >
+              <option value='devops'>Devops</option>
+              <option value='mern'>MERN</option>
+            </select>
+          </div>
+          <div className='my-10 m-auto w-40'>
+            <button className='px-6 py-3 bg-[#E5BE7F] text-xl rounded-lg font-bold'>
+              Submit
+            </button>
+          </div>
         </form>
+      </div>
     </div>
   )
 }
 
 export default Contact
+
--- src/components/Views/Header.jsx
+++ src/components/Views/Header.jsx
@@ -4,17 +4,21 @@ const Header = () => {
   return (
     <div className='wrapper  bg-[#0C3948]'>
       <div className='navBar flex justify-between items-center w-[98%] m-auto py-3'>
-        <div className="logo w-96 flex items-center">
-          <img src={imgUrl} alt="logo" width='128px'/>
-          <h1 className='text-slate-100 font-bold'>Docent Disciple Dream Technologies</h1>
+        <div className='logo w-96 flex items-center'>
+          <img src={imgUrl} alt='logo' width='128px' />
+          <h1 className='text-slate-100 font-bold'>
+            Docent Disciple Dream Technologies
+          </h1>
         </div>
-        <ul className='flex text-slate-100 w-80 justify-between font-bold'>
+        <ul className='flex text-slate-100 w-80 justify-between font-bold cursor-pointer'>
           <li>Home</li>
           <li>About</li>
           <li>Contact</li>
         </ul>
         <div>
-          <button className='px-4 py-2 font-bold rounded bg-[#E5BE7F]'>Join Now</button>
+          <button className='px-4 py-2 font-bold rounded bg-[#E5BE7F] cursor-pointer'>
+            Join Now
+          </button>
         </div>
       </div>
     </div>
