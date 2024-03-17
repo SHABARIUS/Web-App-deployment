954766f62ec0e7048bd6b074fdd697aeb5479930
praveen pugazh <praveenpugazh14@gmail.com>
2024-03-17 12:00:00 +0530

    feat: responsive changes

--- src/components/Views/Courses.jsx
+++ src/components/Views/Courses.jsx
@@ -4,86 +4,105 @@ import salesForce from '../../assets/Salesforce.png'
 // import salesForceSyllabus from '../../assets/Salesforce-Syllabus.pdf'
 // import mernSyllabus from '../../assets/MERN-Syllabus.pdf'
 // import devopsSyllabus from '../../assets/DevOps-Syllabus.pdf'
-import Modal from "./Modal"
+import Modal from './Modal'
 import { useState } from 'react'
 
 const Courses = () => {
-  const[showModal, setShowModal] = useState(false);
+  const [showModal, setShowModal] = useState(false)
   const [syllabus, setSyllabus] = useState('')
   return (
     <>
-    {showModal && <Modal syllabus={syllabus} setShowModal={setShowModal}/>}
-    <div className='w-[85%] m-auto'>
-      <h2 className='text-6xl font-bold my-32'>Courses</h2>
-      <div className='flex justify-around items-center'>
-      <div className='w-96 flex flex-col items-center my-20 p-4 rounded-md shadow-sm hover:shadow-md cursor-pointer'>
-        <div className='w-[95%]'>
-          <img src={devOps} alt='course logo' width='350px' />
-        </div>
-        <div>
-          <h2 className='text-3xl font-bold my-10'>Devops</h2>
-          <h4 className='text-gray-700'>
-            Learn the latest technologies and best practices from industry
-            professionals and gain hands-on experience
-          </h4>
-          <p>
-            <span className='font-bold'> Duration:</span> 3 Months
-          </p>
-          <p>
-            <span className='font-bold'> Mode:</span> Online
-          </p>
-            <button className='my-4 px-2 py-4 bg-[#E5BE7F] rounded-md' onClick={() => {
-              setShowModal(true)
-              setSyllabus('DevOps-Syllabus')
-              }}>Download Syllabus</button>
+      {showModal && <Modal syllabus={syllabus} setShowModal={setShowModal} />}
+      <div className='w-[85%] m-auto'>
+        <h2 className='text-6xl font-bold my-32 text-center md:text-left'>
+          Courses
+        </h2>
+        <div className='flex flex-col md:flex-row justify-center md:justify-around items-center'>
+          <div className='w-96 flex flex-col justify-center items-center my-20 p-4 rounded-md shadow-sm hover:shadow-md cursor-pointer'>
+            <div className='w-[95%]'>
+              <img src={devOps} alt='course logo' width='350px' />
+            </div>
+            <div>
+              <h2 className='text-3xl font-bold my-10'>Devops</h2>
+              <h4 className='text-gray-700'>
+                Learn the latest technologies and best practices from industry
+                professionals and gain hands-on experience
+              </h4>
+              <p>
+                <span className='font-bold'> Duration:</span> 3 Months
+              </p>
+              <p>
+                <span className='font-bold'> Mode:</span> Online
+              </p>
+              <button
+                className='my-4 px-2 py-4 bg-[#E5BE7F] rounded-md'
+                onClick={() => {
+                  setShowModal(true)
+                  setSyllabus('DevOps-Syllabus')
+                }}
+              >
+                Download Syllabus
+              </button>
+            </div>
+          </div>
+          <div className='w-96 flex flex-col items-center my-20 p-4 rounded-md shadow-sm hover:shadow-md cursor-pointer'>
+            <div className='w-[95%]'>
+              <img src={mern} alt='course logo' width='350px' />
+            </div>
+            <div>
+              <h2 className='text-3xl font-bold my-10'>
+                FullStack Development (MERN){' '}
+              </h2>
+              <h4 className='text-gray-700'>
+                We offer comprehensive courses on full stack development to help
+                you advance your career
+              </h4>
+              <p>
+                <span className='font-bold'> Duration:</span> 3 Months
+              </p>
+              <p>
+                <span className='font-bold'> Mode:</span> Online
+              </p>
+              <button
+                className='my-4 px-2 py-4 bg-[#E5BE7F] rounded-md'
+                onClick={() => {
+                  setSyllabus('MERN-Syllabus')
+                  setShowModal(true)
+                }}
+              >
+                Download Syllabus
+              </button>
+            </div>
+          </div>
+          <div className='w-96 flex flex-col items-center my-20 p-4 rounded-md shadow-sm hover:shadow-md cursor-pointer'>
+            <div className='w-[95%]'>
+              <img src={salesForce} alt='course logo' width='300px' />
+            </div>
+            <div>
+              <h2 className='text-3xl font-bold my-10'>Salesforce Admin</h2>
+              <h4 className='text-gray-700'>
+                We offer comprehensive courses on Salesforce admin to help you
+                advance your career
+              </h4>
+              <p>
+                <span className='font-bold'> Duration:</span> 3 Months
+              </p>
+              <p>
+                <span className='font-bold'> Mode:</span> Online
+              </p>
+              <button
+                className='my-4 px-2 py-4 bg-[#E5BE7F] rounded-md'
+                onClick={() => {
+                  setSyllabus('Salesforce-Syllabus')
+                  setShowModal(true)
+                }}
+              >
+                Download Syllabus
+              </button>
+            </div>
+          </div>
         </div>
       </div>
-      <div className='w-96 flex flex-col items-center my-20 p-4 rounded-md shadow-sm hover:shadow-md cursor-pointer'>
-        <div className='w-[95%]'>
-          <img src={mern} alt='course logo' width='350px' />
-        </div>
-        <div>
-          <h2 className='text-3xl font-bold my-10'>FullStack Development (MERN) </h2>
-          <h4 className='text-gray-700'>
-            We offer comprehensive courses on full stack development to help you
-            advance your career
-          </h4>
-          <p>
-            <span className='font-bold'> Duration:</span> 3 Months
-          </p>
-          <p>
-            <span className='font-bold'> Mode:</span> Online
-          </p>
-            <button className='my-4 px-2 py-4 bg-[#E5BE7F] rounded-md' onClick={() => {
-              setSyllabus('MERN-Syllabus')
-              setShowModal(true)
-              }}>Download Syllabus</button>
-        </div>
-      </div>
-      <div className='w-96 flex flex-col items-center my-20 p-4 rounded-md shadow-sm hover:shadow-md cursor-pointer'>
-        <div className='w-[95%]'>
-          <img src={salesForce} alt='course logo' width='300px' />
-        </div>
-        <div>
-          <h2 className='text-3xl font-bold my-10'>Salesforce Admin</h2>
-          <h4 className='text-gray-700'>
-            We offer comprehensive courses on Salesforce admin to help you
-            advance your career
-          </h4>
-          <p>
-            <span className='font-bold'> Duration:</span> 3 Months
-          </p>
-          <p>
-            <span className='font-bold'> Mode:</span> Online
-          </p>
-            <button className='my-4 px-2 py-4 bg-[#E5BE7F] rounded-md' onClick={() => {
-              setSyllabus('Salesforce-Syllabus')
-              setShowModal(true)
-              }}>Download Syllabus</button>
-        </div>
-      </div>
-      </div>
-    </div>
     </>
   )
 }
--- src/components/Views/Landing.jsx
+++ src/components/Views/Landing.jsx
@@ -1,22 +1,32 @@
-import landingImage from "../../assets/landing.svg"
+import landingImage from '../../assets/landing.svg'
 const Landing = () => {
   return (
     <>
-    <div className="h-[900px] w-[85%] m-auto flex flex-col md:flex-row justify-between ">
-        <div className="flex items-center justify-center sm:w-96 md:w-[700px]">
-            <img src={landingImage} alt="coding person" width='700px' />
+      <div className='h-[900px] w-[85%] m-auto flex flex-col md:flex-row justify-between '>
+        <div className='flex mt-5 md:mt-2 items-center justify-center sm:w-96 md:w-[700px]'>
+          <img src={landingImage} alt='coding person' width='700px' />
         </div>
-        <div className="mr-20 mt-60 ">
-            <h1 className='text-4xl lg:text-6xl xl:text-7xl font-bold mb-5 text-center'> Unlock Your <span className="text-[#E5BE7F]">Full Potential</span></h1>
-            <h1 className='text-3xl lg:text-5xl font-bold my-3 text-center md:text-right'>Get The Right Job<span className="text-[#E5BE7F]">.</span></h1>
-            <h3 className='text-3xl my-5 text-gray-700 mt-10 text-center md:text-right'>Join us to learn more about it</h3>
-            <div className="mt-20 text-center md:text-right">
-                <button className='px-12 py-6 bg-[#E5BE7F] text-3xl rounded-lg font-bold'><a  href="#contactForm">Join Now</a></button>
-            </div>
+        <div className='mr-2 md:mr-20 mt-5 md:mt-60'>
+          <h1 className='sm:text-3xl lg:text-6xl xl:text-7xl font-bold mb-5 text-center md:text-right'>
+            {' '}
+            Unlock Your <span className='text-[#E5BE7F]'>Full Potential</span>
+          </h1>
+          <h1 className='text-3xl lg:text-5xl font-bold my-3 text-center md:text-right'>
+            Get The Right Job<span className='text-[#E5BE7F]'>.</span>
+          </h1>
+          <h3 className='text-3xl my-5 text-gray-700 mt-10 text-center md:text-right'>
+            Join us to learn more about it
+          </h3>
+          <div className='mt-20 text-center md:text-right'>
+            <button className='px-8 py-3 md:px-10 md:py-5 lg:px-12 lg:py-6 bg-[#E5BE7F] text-3xl rounded-lg font-bold'>
+              <a href='#contactForm'>Join Now</a>
+            </button>
+          </div>
         </div>
-    </div>
+      </div>
     </>
   )
 }
 
 export default Landing
+
